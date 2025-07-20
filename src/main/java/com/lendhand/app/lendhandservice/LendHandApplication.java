275451eaa26e90562d30bb1422b86e2b9d59129b
nkropinoff <nikkropinoff@gmail.com>
2025-07-20 23:45:48 +0300

    Initial commit

---
+++ src/main/java/com/lendhand/app/lendhandservice/LendHandApplication.java
@@ -0,0 +1,13 @@
+package com.lendhand.app.lendhandservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendHandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LendHandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lendhand/app/lendhandservice/LendHandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lendhand.app.lendhandservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendHandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
