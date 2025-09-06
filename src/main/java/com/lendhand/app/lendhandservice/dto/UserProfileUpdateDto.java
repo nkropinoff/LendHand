db6965bf755885cac7646f90ca1caa56bd910073
nkropinoff <nikkropinoff@gmail.com>
2025-09-06 18:45:27 +0300

    Fix max size of location information (from 20 to 100)

--- src/main/java/com/lendhand/app/lendhandservice/dto/UserProfileUpdateDto.java
+++ src/main/java/com/lendhand/app/lendhandservice/dto/UserProfileUpdateDto.java
@@ -7,12 +7,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserProfileUpdateDto {
-    @Size(max = 20, message = "Информация о местоположении не должна превышать 100 символов.")
+    @Size(max = 100, message = "Информация о местоположении не должна превышать 100 символов.")
     private String location;
 
     @Size(max = 500, message = "Описание не должно превышать 500 символов.")
     private String about;
 
-    @Size(max = 255)
+    @Size(max = 255, message = "URL аватара не должен превышать 255 символов.")
     private String avatarUrl;
 }
