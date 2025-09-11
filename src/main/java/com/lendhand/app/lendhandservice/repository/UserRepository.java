fce08f926144081280e21bee3dc92c2fb3c1d381
nkropinoff <nikkropinoff@gmail.com>
2025-09-11 19:12:28 +0300

    Add @Query to findByEmail method of UserRepository to escape Lazy Initialization problem during avatar displaying in navigation bar

--- src/main/java/com/lendhand/app/lendhandservice/repository/UserRepository.java
+++ src/main/java/com/lendhand/app/lendhandservice/repository/UserRepository.java
@@ -2,13 +2,16 @@ package com.lendhand.app.lendhandservice.repository;
 
 import com.lendhand.app.lendhandservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.userProfile WHERE u.email = :email")
+    Optional<User> findByEmail(@Param("email")  String email);
 
     Optional<User> findByUsername(String username);
 
